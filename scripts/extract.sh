41332757365ba5df202a94e2c1ed990c9f3af84e
David Huggins-Daines <dhd@ecolingui.ca>
2025-06-27 11:24:50 -0400

    feat: use docling models for Laval as it has no structure

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -6,5 +6,5 @@ alexi -v extract -m download/index.json -o export/vdsa download/*.pdf
 alexi -v extract -m download/vsadm/index.json -o export/vsadm download/vsadm/*.pdf
 alexi -v extract -m download/vss/index.json -o export/vss download/vss/*.pdf
 alexi -v extract -m download/prevost/index.json -o export/prevost download/prevost/*.pdf
-alexi -v extract -m download/laval/index.json -o export/laval download/laval/*.pdf
+alexi -v extract -O docling -m download/laval/index.json -o export/laval download/laval/*.pdf
 echo '<meta http-equiv="refresh" content="0; url=vdsa/index.html">' > export/index.html
